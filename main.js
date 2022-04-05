881b8599fdcda596d6422b0183b045460c4a2169
LeeChangHwan <changhwanlee94@gmail.com>
2022-04-05 20:44:18 +0900

    🚧  side-bar open & close 기능 구현

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+const sidebar = document.querySelector('.side-bar')
+const sidebarOverlay = document.querySelector('.overlay')
+
 const toggleSearchInput = () => {
   const inputGroup = document.querySelector('.input-group')
   if (inputGroup.style.display == 'block') {
@@ -5,4 +8,14 @@ const toggleSearchInput = () => {
   } else {
     inputGroup.style.display = 'block'
   }
+}
+
+const openSidebar = () => {
+  sidebar.style.width = '50vw'
+  sidebarOverlay.style.display = 'block'
+}
+
+const closeSidebar = () => {
+  sidebar.style.width = '0'
+  sidebarOverlay.style.display = 'none'
 }
