f5403552bd59d44a2d75f2a01efa211c243313ca
LeeChangHwan <changhwanlee94@gmail.com>
2022-04-05 20:09:45 +0900

    🚧  Implement toggling .input-group

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-
+const toggleSearchInput = () => {
+  const inputGroup = document.querySelector('.input-group')
+  if (inputGroup.style.display == 'block') {
+    inputGroup.style.display = 'none'
+  } else {
+    inputGroup.style.display = 'block'
+  }
+}
